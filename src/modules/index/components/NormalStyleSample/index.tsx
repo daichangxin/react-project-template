c5c0768a34d99b0db3ce152b8c470f6fdfa57fd3
daichangxin <daichangxin@gmail.com>
2022-09-02 14:26:16 +0800

    feat: add sm layout fix: tailwindcss sm not work

--- src/modules/index/components/NormalStyleSample/index.tsx
+++ src/modules/index/components/NormalStyleSample/index.tsx
@@ -5,7 +5,7 @@ export const NormalStyleSample: FC = () => {
     return (
         <div className={styles.container}>
             Just do it!
-            <div className="flex ">
+            <div className="flex flex-col sm:flex-row">
                 <button className="bg-avatar-default w-80 h-80 bg-no-repeat" />
                 <button className="bg-[url('/src/assets/images/avatars/default.png')] w-80 h-80 bg-no-repeat" />
             </div>
